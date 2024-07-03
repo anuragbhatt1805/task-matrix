35fd2b4822e6ac8908419c90bd10f4e0e074aa25
raunak234362 <raunakdob@gmail.com>
2024-07-03 16:07:57 +0530

    `feat: Add @syncfusion/ej2-react-schedule dependency and integrate calendar component`

--- src/components/dashboard/calendar/Calendar.jsx
+++ src/components/dashboard/calendar/Calendar.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Inject,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  ScheduleComponent,
+} from "@syncfusion/ej2-react-schedule";
 const Calendar = () => {
   return (
     <div>
-     <div>
-        <h1 className="text-2xl flex font-bold uppercase bg-slate-500 text-white px-5 py-2 justify-center items-center">Calendar</h1>
+      <div>
+        <h1 className="text-2xl flex font-bold uppercase bg-slate-500 text-white px-5 py-2 justify-center items-center">
+          Calendar
+        </h1>
+      </div>
+      <div>
+        <ScheduleComponent>
+            <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+        </ScheduleComponent>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Calendar
+export default Calendar;
